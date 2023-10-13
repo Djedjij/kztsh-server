f27b2dbf1f4f2c5b8e43f6ca7ba82ef884cd96b7
Djedjij <mat3347571@gmail.com>
2023-10-13 15:22:54 +0300

    131023

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,17 +1,32 @@
 const ApiError = require("../error/ApiError");
-const bcrypt = require("bcryptjs");
 const User = require("../models/models");
-
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 class authController {
-  async login(req, res) {
+  async login(req, res, next) {
     try {
       const { username, password } = req.body;
+      const user = await User.findOne({ username });
+      if (!user) {
+        throw new Error("User not found");
+      }
+      const isValidPassword = await bcrypt.compare(password, user.password);
+      if (!isValidPassword) {
+        throw new Error("Invalid password");
+      }
+      const token = jwt.sign({ id: user._id }, "your_secret_key", {
+        expiresIn: "1h",
+      });
+      return res.json({
+        message: "Login successful",
+        token,
+      });
     } catch (e) {
       next(ApiError.badRequest(e.message));
     }
   }
 
-  async getUsers(req, res) {
+  async getUsers(req, res, next) {
     try {
       return res.json("server work");
     } catch (e) {
